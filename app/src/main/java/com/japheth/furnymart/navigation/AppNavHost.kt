bc872ffa8a23b855b4e4aee7238b4af9a09e9e06
srstig <rayogutu36@gmailcom>
2025-04-28 10:05:06 +0300

    furni commit

---
+++ app/src/main/java/com/japheth/furnymart/navigation/AppNavHost.kt
@@ -0,0 +1,37 @@
+package com.japheth.furnymart.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.japheth.furnymart.screens.about.AboutScreen
+import com.japheth.furnymart.screens.contact.ContactScreen
+import com.japheth.furnymart.screens.home.HomeScreen
+
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_HOME
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_HOME) {
+            HomeScreen(navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController)
+        }
+        composable(ROUT_CONTACT) {
+            ContactScreen(navController)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/japheth/furnymart/navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.japheth.furnymart.navigation
+
+const val ROUT_HOME ="home"
+const val ROUT_ABOUT ="about"
+const val ROUT_CONTACT ="contact"
